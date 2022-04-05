f63d2fe0b6f2f009fc96867f1a15793b592668ee
jessicaklier <jhessicadesouza12@hotmail.com>
2022-04-05 14:51:43 -0300

    ProductDetails layout mobile

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,8 +1,34 @@
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice';
+import './styles.css';
 
 const ProductDetails = () => {
-    return (
-        <h1>Página ProductDetails</h1>
-    );
-}
+  return (
+    <div className="product-details-container">
+      <div className="base-card product-details-card">
+        <div className="goback-container">
+            <ArrowIcon /> 
+            <h2>VOLTAR</h2>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xl-6"></div>
+        <div className="img-container">
+            <img src="https://raw.githubusercontent.com/jhessicaklier/dscatalog-resource/master/backend/img/2-big.jpg" alt="Nome do Produto" />
+        </div>
+        <div className="name-price-container">
+            <h1>Nome do Produto</h1>
+            <ProductPrice price={2345.67} />
+        </div>
+        <div className="col-xl-6">
+          <div className="description-container">
+              <h2>Descriçao do produto</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, esse.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProductDetails;
